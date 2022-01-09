42b15ccf9201ac2b5c58e77e59af4f598c9ab1f2
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-01-09 13:38:05 +0900

    Chore : Prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 80,
+  quoteProps: 'consistent',
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+};
