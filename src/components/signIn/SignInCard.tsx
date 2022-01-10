6bbd9c4525295904d118c6932cad4c4b761de279
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-01-11 00:45:20 +0900

    Design : 로그인 페이지 마크업 및 스타일링 구현

---
+++ src/components/signIn/SignInCard.tsx
@@ -0,0 +1,193 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const SignInCard: React.FC = () => {
+  return (
+    <Container>
+      <CardHead>로그인</CardHead>
+      <SignInForm>
+        <SignInLabel>이메일</SignInLabel>
+        <SignInInput placeholder="이메일" />
+        <SignInLabel>비밀번호</SignInLabel>
+        <SignInInput placeholder="비밀번호" />
+        <Options>
+          <IdSaveCheckBox>
+            <IdSaveCheckInput type="checkbox" id="idSave" name="idSave" />
+            <IdSaveCheckLabel htmlFor="idSave">아이디 저장</IdSaveCheckLabel>
+          </IdSaveCheckBox>
+          <AccountManagementMenu>
+            <Link to="#" css={accountLink}>
+              아이디 찾기
+            </Link>
+            <Link to="#" css={accountLink}>
+              비밀번호 찾기
+            </Link>
+          </AccountManagementMenu>
+        </Options>
+        <SignInButton>로그인</SignInButton>
+        <SignUpLinkBox>
+          아직 계정이 없으신가요?{' '}
+          <Link to="/users/sign_up" className="sign-up-link">
+            계정 만들기
+          </Link>
+        </SignUpLinkBox>
+      </SignInForm>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 460px;
+  margin: 0 auto;
+  padding: 56px;
+  border: 1px solid #d7e2eb;
+  border-radius: 6px;
+`;
+
+const CardHead = styled.h3`
+  margin-bottom: 56px;
+  font-size: 26px;
+  font-weight: 600;
+  text-align: center;
+`;
+
+const SignInForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SignInLabel = styled.label`
+  margin-bottom: 4px;
+  font-size: 14px;
+  line-height: 1.5;
+  color: #263747;
+`;
+
+const SignInInput = styled.input`
+  width: 100%;
+  height: 40px;
+  margin-bottom: 16px;
+  padding: 8px 12px;
+  outline: 0;
+  border: 1px solid #d7e2eb;
+  border-radius: 0.25rem;
+  box-sizing: border-box;
+  background-color: #fbfbfd;
+  font-size: 16px;
+  line-height: 24px;
+  color: #263747;
+  transition: all 0.08s ease-in-out;
+
+  &:hover {
+    border: 1px solid #3396f4;
+    box-shadow: inset 0 0 0 1px#3396f4;
+  }
+  &:focus {
+    border: 1px solid #3396f4;
+    box-shadow: inset 0 0 0 1px #3396f4;
+    background-color: #fff;
+    color: #495057;
+  }
+`;
+
+const IdSaveCheckBox = styled.div`
+  margin-bottom: 16px;
+`;
+
+const Options = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const IdSaveCheckInput = styled.input``;
+
+const IdSaveCheckLabel = styled.label`
+  font-size: 14px;
+  color: #98a8b9;
+  transition: color 0.08s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    color: #3396f4;
+  }
+`;
+
+const AccountManagementMenu = styled.div``;
+
+const SignInButton = styled.button`
+  height: 40px;
+  margin-bottom: 24px;
+  border: none;
+  border-radius: 0.25rem;
+  box-sizing: border-box;
+  background-color: #3396f4;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  transition: background-color 0.08s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #2878c3;
+  }
+`;
+
+const SignUpLinkBox = styled.div`
+  font-size: 14px;
+  line-height: 1.5;
+  color: #98a8b9;
+  text-align: center;
+
+  .sign-up-link {
+    padding-left: 4px;
+    font-weight: 500;
+    color: #263747;
+
+    &::after {
+      content: '〉';
+      display: inline-block;
+      margin-left: 0.25rem;
+      vertical-align: top;
+      transition: all 0.08s ease-in-out;
+      transition-delay: initial;
+    }
+    &:hover {
+      color: #2878c3;
+
+      &::after {
+        content: '〉';
+        transform: translateX(3px);
+      }
+    }
+  }
+`;
+
+const accountLink = css`
+  font-size: 14px;
+  color: #98a8b9;
+  transition: color 0.08s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    color: #3396f4;
+  }
+
+  &:first-of-type {
+    &:after {
+      content: '';
+      display: inline-block;
+      width: 1px;
+      height: 10px;
+      margin: 0 8px;
+      background-color: #98a8b9;
+    }
+  }
+`;
+
+export default SignInCard;
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+import Header from '../components/common/Header';
+import SignInCard from '../components/signIn/SignInCard';
+import Footer from '../components/common/Footer';
+
 const SignInPage: React.FC = () => {
-  return <div>로그인 페이지</div>;
+  return (
+    <Container>
+      <Header />
+      <Wrapper>
+        <SignInCard />
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  max-width: 1200px;
+  margin-top: 160px;
+  padding: 0 16px;
+`;
 
 export default SignInPage;
