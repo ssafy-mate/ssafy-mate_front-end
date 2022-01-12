3ded58fd74f4d2143371496e18a97431ddfe19b1
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-01-12 11:45:03 +0900

    Design : 프로젝트 페이지네이션 반응형 스타일링 적용

---
+++ src/components/projects/ProjectNavigation.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import HowToVoteIcon from '@mui/icons-material/HowToVote';
+
+const ProjectNavigation: React.FC = () => {
+  return (
+    <NavTabs>
+      <NavItem className="active">
+        <Link to="/projects/specialization/team" css={navLink}>
+          <ContentPasteSearchIcon css={navLinkIcon} />팀 공고
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link to="/projects/specialization/student" css={navLink}>
+          <PersonSearchIcon css={navLinkIcon} />
+          교육생 공고
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link to="/" css={navLink}>
+          <VolunteerActivismIcon css={navLinkIcon} />
+          받은 제안
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link to="#" css={navLink}>
+          <HowToVoteIcon css={navLinkIcon} />
+          지원한 팀
+        </Link>
+      </NavItem>
+    </NavTabs>
+  );
+};
+
+const NavTabs = styled.ul`
+  display: flex;
+  justify-content: center;
+  margin: 48px 0 24px;
+  border-color: #fff;
+  box-shadow: inset 0 -0.0625rem #d7e2eb;
+`;
+
+const NavItem = styled.li`
+  width: 96px;
+  color: #263747;
+  transition: all 0.08s ease-in-out;
+
+  &:hover {
+    color: #3396f4;
+  }
+  &.active {
+    box-shadow: inset 0 -0.1875rem #3396f4;
+    color: #3396f4;
+  }
+`;
+
+const navLink = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 0;
+  font-size: 16px;
+  line-height: 1.6;
+
+  @media (max-width: 540px) {
+    font-size: 14px;
+  }
+`;
+
+const navLinkIcon = css`
+  margin-bottom: 4px;
+
+  @media (max-width: 540px) {
+    display: none;
+  }
+`;
+
+export default ProjectNavigation;
