b5d755d7c45fb8babd630c45cd48e5dddadec03f
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-01-14 10:23:43 +0900

    Feat : 팀 수정하기 페이지 추가

---
+++ src/pages/EditTeamPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import styled from '@emotion/styled';
+
+import Header from '../components/common/Header';
+import ProjectNavigation from '../components/projects/ProjectNavigation';
+import EditTeamForm from '../components/team/EditTeamForm';
+import Footer from '../components/common/Footer';
+
+const EditTeamPage: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <ProjectNavigation />
+      <Container>
+        <EditTeamForm />
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+const Container = styled.div`
+  margin-top: 80px;
+  padding: 0 16px;
+
+  @media (max-width: 580px) {
+    margin-top: 60px;
+  }
+  @media (max-width: 414px) {
+    margin-top: 40px;
+  }
+`;
+
+export default EditTeamPage;
