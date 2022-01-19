0e84bce6de7c9e596118215daa9bb6f6ed83631f
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-01-19 11:48:47 +0900

    Set : Mock Service Worker 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,12 @@ import createReduxStore from './redux/createReduxStore';
 
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+
+  worker.start();
+}
+
 const store = createReduxStore();
 
 ReactDOM.render(
