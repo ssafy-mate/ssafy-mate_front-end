8d6ad1628913af10f12ffa65b8413854d3a3a400
JeongHwan-dev <jeonghwan.dev@gmail.com>
2022-02-03 00:34:39 +0900

    Fix : 리듀서 설정 오류 수정

--- src/redux/modules/reducer.ts
+++ src/redux/modules/reducer.ts
@@ -1,27 +1,29 @@
 import { connectRouter } from 'connected-react-router';
 
+// import { History } from 'history';
+
 import { combineReducers } from 'redux';
 
 import { persistReducer } from 'redux-persist';
 
 import storage from 'redux-persist/lib/storage';
 
-import history from '../../history';
-
 import auth from './auth';
 import user from './user';
+
+import history from '../../history';
 
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['auth'],
+  blacklist: ['router'],
 };
 
-const reducer = (history: History<unknown>) =>
-  combineReducers({
-    auth,
-    user,
-    router: connectRouter(history),
-  });
+const reducer = combineReducers({
+  auth,
+  user,
+  router: connectRouter(history),
+});
 
 export default persistReducer(persistConfig, reducer);
